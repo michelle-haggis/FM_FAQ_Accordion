f039efbbf0b59ba196cecab0744f8cc089833c2c
michelle-haggis <haggis.michelle@gmail.com>
2024-02-17 13:29:19 +0200

    done

--- js/scriptc.js
+++ js/scriptc.js
@@ -1,16 +1,31 @@
-// const faqs = document.querySelectorAll(".faq");
-// faqs.forEach((faq) => {
-//     faq.addEventListener("click", () => {
-//         faq.classList.toggle("active");
-//     });
-// });
 const faqs = document.querySelectorAll(".faq");
+
+// Open the first FAQ by adding the "active" class
+faqs[0].classList.add("active");
 
 faqs.forEach((faq) => {
     const openIcon = faq.querySelector(".open");
     const closeIcon = faq.querySelector(".close");
 
+    if (faq.classList.contains("active")) {
+        openIcon.style.display = "none";
+        closeIcon.style.display = "inline-block";
+    } else {
+        openIcon.style.display = "inline-block";
+        closeIcon.style.display = "none";
+    }
+
     faq.addEventListener("click", () => {
+        faqs.forEach((otherFaq) => {
+            if (otherFaq !== faq) {
+                otherFaq.classList.remove("active");
+                const otherOpenIcon = otherFaq.querySelector(".open");
+                const otherCloseIcon = otherFaq.querySelector(".close");
+                otherOpenIcon.style.display = "inline-block";
+                otherCloseIcon.style.display = "none";
+            }
+        });
+
         faq.classList.toggle("active");
 
         if (faq.classList.contains("active")) {
